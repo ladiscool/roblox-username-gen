c8249c38046e9cbe03939f0820d31889df5040f4
novak <81375632+novak5712@users.noreply.github.com>
2023-06-21 20:17:32 +0600

    compiler for linux and win

--- compile-ubuntu.sh
+++ compile-ubuntu.sh
@@ -1,1 +1,11 @@
-go build src/main.go && chmod +x main && ./main
+#!/bin/bash
+
+go build src/main.go
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+  executable="./main.exe"
+else
+  executable="./main"
+  chmod +x "$executable"
+fi
+"$executable"
